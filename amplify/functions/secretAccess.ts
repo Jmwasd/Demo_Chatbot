0f8c8e402a27c30f4cc7022281c794b763275e6f
minwoo <minwoo95>
2025-12-11 11:07:31 +0900

    added secret value

--- amplify/functions/secretAccess.ts
+++ amplify/functions/secretAccess.ts
@@ -3,5 +3,7 @@ import { defineFunction, secret } from "@aws-amplify/backend";
 export const sayHello = defineFunction({
   environment: {
     DATABASE_URL: secret("DATAVASE_URL"),
+    GOOGLE_API_KEY: secret("GOOGLE_API_KEY"),
+    IDENTITY_POOL_ID: secret("IDENTITY_POOL_ID"),
   },
 });
